23c6b8cfba18e8e7cb891b07f8cddc8b1abf331b
ilyapc <ilya.komendantov@list.ru>
2024-03-31 18:54:26 +0300

    added FCM

--- app/src/main/java/ru/kraz/pushonesignal/MainActivity.kt
+++ app/src/main/java/ru/kraz/pushonesignal/MainActivity.kt
@@ -1,11 +1,17 @@
 package ru.kraz.pushonesignal
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
 import androidx.lifecycle.lifecycleScope
+import com.google.firebase.messaging.FirebaseMessaging
 import com.huawei.hms.aaid.HmsInstanceId
+import com.huawei.hms.api.HuaweiApiAvailability
 import com.huawei.hms.common.internal.HmsClient
 import com.huawei.hms.push.HmsMessaging
 import com.onesignal.notifications.bridges.OneSignalHmsEventBridge
@@ -13,15 +19,37 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity()  {
+class MainActivity : AppCompatActivity() {
+
+    private val permission = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         lifecycleScope.launch(Dispatchers.IO) {
-            val token = HmsInstanceId.getInstance(this@MainActivity).getToken("110274235", "HCM")
-            HmsMessaging.getInstance(this@MainActivity).subscribe("topic1")
+            val phoneIs = HuaweiApiAvailability.getInstance()
+                .isHuaweiMobileServicesAvailable(this@MainActivity)
+            if (phoneIs == 1) {
+                FirebaseMessaging.getInstance().subscribeToTopic("topic1")
+            } else {
+                val token = HmsInstanceId.getInstance(this@MainActivity).getToken("110274235", "HCM")
+                HmsMessaging.getInstance(this@MainActivity).subscribe("topic1")
+            }
+
             //OneSignalHmsEventBridge.onNewToken(this@MainActivity, token)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.POST_NOTIFICATIONS
+                ) == PackageManager.PERMISSION_DENIED
+            ) {
+                permission.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+            }
         }
     }
 }
--- app/src/main/java/ru/kraz/pushonesignal/MyFirebaseMessagingService.kt
+++ app/src/main/java/ru/kraz/pushonesignal/MyFirebaseMessagingService.kt
@@ -8,18 +8,17 @@ import android.content.pm.PackageManager
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
-import com.huawei.hms.push.HmsMessageService
-import com.huawei.hms.push.RemoteMessage
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
-class MyHmsMessagingService : HmsMessageService() {
+class MyFirebaseMessagingService : FirebaseMessagingService() {
 
-    override fun onNewToken(p0: String?) {
-        super.onNewToken(p0)
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
     }
 
-    override fun onMessageReceived(message: RemoteMessage?) {
+    override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
-        //OneSignalHmsEventBridge.onMessageReceived(this@MyHmsMessagingService, p0)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             if (ContextCompat.checkSelfPermission(
@@ -34,8 +33,8 @@ class MyHmsMessagingService : HmsMessageService() {
         }
     }
 
-    private fun showNotification(message: RemoteMessage?) {
-        message?.dataOfMap?.let { data ->
+    private fun showNotification(message: RemoteMessage) {
+        message.notification?.let { notificationReceived ->
             val intent = Intent(this, MainActivity::class.java).apply {
                 flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
             }
@@ -49,20 +48,15 @@ class MyHmsMessagingService : HmsMessageService() {
             val notificationManager =
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-            val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            val notification = NotificationCompat.Builder(this, MyHmsMessagingService.CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle(data["title"])
-                .setContentText(data["body"])
+                .setContentTitle(notificationReceived.title)
+                .setContentText(notificationReceived.body)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true)
 
             notificationManager.notify(1, notification.build())
         }
-    }
-
-    companion object {
-        const val CHANNEL_ID = "CHANNEL_ID"
-        const val CHANNEL_NAME = "CHANNEL_NAME"
     }
 }
